b3cb295b6ceb08d9535c7938427ffea44e4ae47e
Gabiru97 <gabriel-g-martins@hotmail.com>
2024-05-23 23:39:25 -0300

    Iniciando o java

---
+++ meu-primeiro-projeto-java/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("------------x-------------");
+        System.out.println("Boas Vindas time turma DIO");
+        System.out.println("------------x-------------");
+
+
+
+
+    }
+}
